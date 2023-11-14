96cd1e7d7aa0d2f88c2dbd8f34180db6a604939c
lwonsang <l.wonsang@wustl.edu>
2023-11-13 22:02:35 -0600

    fdasfds

--- StudyRoomRater/StudyRoomRater/ReviewsViewController.swift
+++ StudyRoomRater/StudyRoomRater/ReviewsViewController.swift
@@ -1,52 +1,50 @@
+////
+////  ReviewsViewController.swift
+////  StudyRoomRater
+////
+////  Created by Wonsang Lee on 11/10/23.
+////
 //
-//  ReviewsViewController.swift
-//  StudyRoomRater
+//import Foundation
+//import UIKit
 //
-//  Created by Wonsang Lee on 11/10/23.
+//class ReviewsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+//    var tableView: UITableView!
+//    var reviewsArray:[Review] = []
 //
-
-import Foundation
-import UIKit
-
-class ReviewsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
-    var tableView: UITableView!
-    var reviewStars: Int!
-    var reviewUsername: String!
-    var review: String!
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: "myCell") as! ReviewViewCell
-        cell.usernameLabel.text = reviewUsername
-        if reviewStars == 5 {
-            
-        }
-        else if reviewStars == 4 {
-            
-        }
-        else if reviewStars == 3{
-            
-        }
-        else if reviewStars == 2{
-            
-        }
-        else if reviewStars == 1{
-            
-        }
-        cell.reviewLabel.text = review
-        return cell
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        navigationItem.title = "Reviews:"
-        tableView = UITableView()
-        tableView.delegate = self
-        tableView.dataSource = self
-        view.addSubview(tableView)
-        tableView.frame = view.bounds
-    }
-}
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 0
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = UITableViewCell(style: .default, reuseIdentifier: "myCell") as! ReviewViewCell
+//
+//        cell.usernameLabel.text = "Username: \(review.username)"
+//        cell.reviewLabel.text = review.comment
+//
+//        for i in 0..<5 {
+//            let starImageView = UIImageView()
+//            starImageView.frame = CGRect(x: i * 20, y: 0, width: 20, height: 20)
+//            starImageView.contentMode = .scaleAspectFit
+//
+//            if i < review.rating {
+//                starImageView.image = UIImage(systemName: "star.fill")
+//            } else {
+//                starImageView.image = UIImage(systemName: "star")
+//            }
+//
+//            cell.contentView.addSubview(starImageView)
+//        }
+//        return cell
+//    }
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        navigationItem.title = "Reviews:"
+//        tableView = UITableView()
+//        tableView.delegate = self
+//        tableView.dataSource = self
+//        view.addSubview(tableView)
+//        tableView.frame = view.bounds
+//    }
+//}
--- StudyRoomRater/StudyRoomRater/StudyRoomSearchViewController.swift
+++ StudyRoomRater/StudyRoomRater/StudyRoomSearchViewController.swift
@@ -1,85 +1,81 @@
+////
+////  StudyRoomSearchViewController.swift
+////  StudyRoomRater
+////
+////  Created by Wonsang Lee on 11/10/23.
+////
 //
-//  StudyRoomSearchViewController.swift
-//  StudyRoomRater
+//import Foundation
+//import UIKit
 //
-//  Created by Wonsang Lee on 11/10/23.
+//class StudyRoomSearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+//    @IBOutlet weak var filtersButton: UIButton!
+//    var tableView: UITableView!
+//    var studyRoomName: String! //revise this base on database
+//    var studyRoomProximity: Int!
+//    var reviewStars: Int!
+//    var studyRoomImage: UIImage!
+//    var studyRoomData: [StudyRoom] = []
+//    var imageCache: [UIImage] = []
 //
-
-import Foundation
-import UIKit
-
-class StudyRoomSearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
-    @IBOutlet weak var filtersButton: UIButton!
-    var tableView: UITableView!
-    var studyRoomName: String! //revise this base on database
-    var studyRoomProximity: Int!
-    var reviewStars: Int!
-    var studyRoomImage: UIImage!
-    var studyRoomData: [StudyRoom] = []
-    var imageCache: [UIImage] = []
-    
-    struct StudyRoom: Decodable {
-        let id: Int!
-        let name: String!
-        let outlets: Int!
-        let occupants: Int!
-        let location: String!
-        let description: String!
-        let buildingId: Int!
-        let roomNumber: Int!
-        
-    }
-    
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: "myCell") as! StudyRoomSearchViewCell
-        cell.studyRoomLabel.text = studyRoomName + " (" + String(studyRoomProximity) + " min)"
-        if reviewStars == 5 {
-            
-        }
-        else if reviewStars == 4 {
-            
-        }
-        else if reviewStars == 3{
-            
-        }
-        else if reviewStars == 2{
-            
-        }
-        else if reviewStars == 1{
-            
-        }
-        cell.studyRoomImageView.image = studyRoomImage
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let studyRoomVC = StudyRoomViewController()
-        //for item in studyRoomData {//revise this
-            studyRoomVC.name = studyRoomName
-            
-        //}
-        
-        navigationController?.pushViewController(studyRoomVC, animated: true)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        navigationItem.title = "Search Results:"
-        tableView = UITableView()
-        tableView.delegate = self
-        tableView.dataSource = self
-        view.addSubview(tableView)
-        tableView.frame = view.bounds
-    }
-    @IBAction func filtersClicked(_ sender: Any) {
-        let filtersVC = FiltersViewController()
-        navigationController?.pushViewController(filtersVC, animated: true)
-        
-    }
-}
+//    struct StudyRoom: Decodable {
+//        let id: Int!
+//        let name: String!
+//        let outlets: Int!
+//        let occupants: Int!
+//        let location: String!
+//        let description: String!
+//        let buildingId: Int!
+//        let roomNumber: Int!
+//
+//    }
+//
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 0
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = UITableViewCell(style: .default, reuseIdentifier: "myCell") as! StudyRoomSearchViewCell
+//        cell.studyRoomLabel.text = studyRoomName + " (" + String(studyRoomProximity) + " min)"
+//        let review = reviewsArray[indexPath.row]
+//
+//        cell.usernameLabel.text = "Username: \(String(describing: review.username))"
+//        cell.reviewLabel.text = review.comment
+//        for i in 0..<5 {
+//            let starImageView = UIImageView()
+//            starImageView.frame = CGRect(x: i * 20, y: 0, width: 20, height: 20)
+//            starImageView.contentMode = .scaleAspectFit
+//            if i < review.rating {
+//                starImageView.image = UIImage(systemName: "star.fill")
+//            } else {
+//                starImageView.image = UIImage(systemName: "star")
+//            }
+//            cell.contentView.addSubview(starImageView)
+//    }
+//
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let studyRoomVC = StudyRoomViewController()
+//        //for item in studyRoomData {//revise this
+//            studyRoomVC.name = studyRoomName
+//
+//        //}
+//
+//        navigationController?.pushViewController(studyRoomVC, animated: true)
+//    }
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        navigationItem.title = "Search Results:"
+//        tableView = UITableView()
+//        tableView.delegate = self
+//        tableView.dataSource = self
+//        view.addSubview(tableView)
+//        tableView.frame = view.bounds
+//    }
+//    @IBAction func filtersClicked(_ sender: Any) {
+//        let filtersVC = FiltersViewController()
+//        navigationController?.pushViewController(filtersVC, animated: true)
+//
+//    }
+//}
